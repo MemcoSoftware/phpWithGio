8606678ad60a647ea76c9b465bda1e0cd6c05b39
MemcoDev <software@memcosas.com>
2023-07-12 16:52:22 -0500

    updates

--- index.php
+++ index.php
@@ -1,42 +1,19 @@
 <?php 
 
-// ARRAY
+/** Error Handling */
 
+function errorHandler(
+    int $type,
+    string $msg,
+    ?string $file = null,
+    ?int $line = null
+){
+    echo $type . ': ' . $msg . ' in ' . $file . ' on line ' . $line;
 
-$projects = [ 
+    exit;
+}
 
-    'ziriuzv2' => [
-        'creator' => 'B4N3D',
-        'frontend' => 'ReactJS',
-        'backend' => 'NodeJS',
-        'dataBase' => 'MongoDB',
-        'Hosting' => 'Amazon Web Services',
-        'versions' => [
-            ['version' => 2.0, 'releaseDate' => 'August 01, 2023'],
-            ['version' => 3.0, 'realeaseDate' => 'September 01, 2023'],
-        ],
-    ],
+set_error_handler('errorHandler', E_ALL);
 
-];
-
-// echo $projects['ziriuzv2']['versions'][0]['releaseDate'];
-
-    foreach($projects['ziriuzv2']as $key => $value) {
-        echo $key . ' - ';
-
-        if(is_array($value)){
-            foreach($value as $skill){
-                echo $skill . '- ';
-            }
-        } else {
-            echo $value;
-        }
-
-        echo '<br />';
-
-    }
-
-    
-
-
+echo $x
 ?>
